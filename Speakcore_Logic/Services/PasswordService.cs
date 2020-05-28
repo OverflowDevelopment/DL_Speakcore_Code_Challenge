b234a005b091b4ebe384ca257b86b4aa94fff0fc
Doug LeBlanc <dleb669@gmail.com>
2020-05-28 18:33:53 -0400

    Adding Logic project to Solution

---
+++ Speakcore_Logic/Services/PasswordService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Speakcore_Logic.Services
+{
+    public static class PasswordService
+    {
+        public static bool IsValidInput(string input)
+        {
+            return false;
+        }
+    }
+}
