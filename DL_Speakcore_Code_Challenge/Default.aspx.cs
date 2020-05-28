5dd2d9108040815f86b4022849f571cae4cb1301
Doug LeBlanc <dleb669@gmail.com>
2020-05-28 18:34:10 -0400

    Plugging in Logic call to web project

--- DL_Speakcore_Code_Challenge/Default.aspx.cs
+++ DL_Speakcore_Code_Challenge/Default.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Speakcore_Logic.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,8 +17,10 @@ namespace DL_Speakcore_Code_Challenge
         protected void btnLogin_Click(object sender, EventArgs e)
         {
             //Get input from txt
+            string input = txtPassword.Text;
 
             //Validate item exists in DB
+            PasswordService.IsValidInput(input);
         }
     }
 }
