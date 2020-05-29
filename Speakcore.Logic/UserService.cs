e9d68f83b84f29f5be4c7ba355a4aebe69245d63
Doug LeBlanc <dleb669@gmail.com>
2020-05-29 17:03:28 -0400

    Adding user service

---
+++ Speakcore.Logic/UserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Speakcore.Data;
+
+namespace Speakcore.Logic
+{
+    public static class UserService
+    {
+        public static void InsertUser(string firstName, string lastName, string state, string email)
+        {
+            DataHelper d = new DataHelper();
+
+            d.InsertUser(firstName, lastName, state, email);
+        }
+    }
+}
