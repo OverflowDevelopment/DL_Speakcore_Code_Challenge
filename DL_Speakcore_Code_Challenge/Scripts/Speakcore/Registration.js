7aff7227a25de1963f2eb02f5ad6480f87aaeed8
Doug LeBlanc <dleb669@gmail.com>
2020-05-31 08:13:00 -0400

    Deleting unwanted validators

---
+++ DL_Speakcore_Code_Challenge/Scripts/Speakcore/Registration.js
@@ -0,0 +1,8 @@
+﻿var inputClass = "sc_light-red";
+var labelClass = "sc_dark-red";
+
+function highlightControl(input, label)
+{
+    $("input").addClass(inputClass);
+    $("label").addClass(labelClass);
+}
